0fc83e9a6027c17eb373db06f1985e3a3ae494e8
Remonjuice <r116303@g.kobe-kosen.ac.jp>
2019-10-07 00:17:10 +0900

    Add class and function

---
+++ Solver/Simulator/Com/com_interface.cpp
@@ -0,0 +1,24 @@
+#include "com_interface.hpp"
+
+class ComInterface
+{
+	int getMatchInfo(MatchInfo *matchInfo)
+	{
+		return 401;
+	}
+
+	int getMatchData(MatchData *matchData)
+	{
+		return 401;
+	}
+
+	int sendActionData(ActionData *actionData)
+	{
+		return 401;
+	}
+
+	int getPimg()
+	{
+		return 401;
+	}
+};
---
+++ Solver/Simulator/Com/com_interface.hpp
@@ -0,0 +1,11 @@
+#include "match_data.hpp"
+#include "match_info.hpp"
+#include "action_data.hpp"
+
+class ComInterface
+{
+	int getMatchInfo(MatchInfo* matchInfo);
+	int getMatchData(MatchData* matchData);
+	int sendActionData(ActionData* actionData);
+	int getPimg();
+};
