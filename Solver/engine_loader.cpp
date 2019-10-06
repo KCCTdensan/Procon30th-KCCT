86fb7f8176d9d7c0c7ff66455152e016bb491853
nekosaburo8250 <r116309@g.kobe-kosen.ac.jp>
2019-10-06 22:54:56 +0900

    update

---
+++ Solver/engine_loader.cpp
@@ -0,0 +1,30 @@
+#include "engine_loader.hpp"
+
+
+namespace solver
+{
+	EngineLoader::EngineLoader(const std::wstring &engineName)
+	{
+		const std::wstring dllPath = L"engine/" + engineName + L".dll";
+		moduleHandle = LoadLibrary(engineName.c_str());
+		if(moduleHandle == NULL)
+		{
+			throw L"EngineManager : DLLの読み込みに失敗しました";
+		}
+		creator = reinterpret_cast<creator_t>(GetProcAddress(moduleHandle, "createEngine"));
+		if(creator == NULL)
+		{
+			throw L"EngineManager : インスタンス生成関数の取得に失敗しました";
+		}
+		destroyer = reinterpret_cast<destroyer_t>(GetProcAddress(moduleHandle, "destroyEngine"));
+		if(destroyer == NULL)
+		{
+			throw L"EngineManager : インスタンス破棄関数の取得に失敗しました";
+		}
+	}
+
+	EngineLoader::~EngineLoader()
+	{
+		FreeLibrary(moduleHandle);
+	}
+}
---
+++ Solver/engine_loader.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "engine/interface.hpp"
+#include <string>
+#include <Windows.h>
+
+
+namespace solver
+{
+	using creator_t = engine::Interface * (*)();
+	using destroyer_t = void(*)(engine::Interface *);
+
+	class EngineLoader
+	{
+		HMODULE moduleHandle;
+		creator_t creator;
+		destroyer_t destroyer;
+		
+	public:
+		EngineLoader(const std::wstring &engineName);
+		~EngineLoader();
+	};
+}
